54bea69cd25795b4ffcaea398babff67c0e5828c
Ramsey Nasser <ram@nas.sr>
2016-06-20 09:42:39 -0400

    Patch AssemblyInfo on build, update REPL

--- Nostrand/Nostrand/Nostrand.cs
+++ Nostrand/Nostrand/Nostrand.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 using clojure.lang;
 using Mono.Terminal;
 
@@ -9,18 +11,18 @@ namespace Nostrand
 	public class Nostrand
 	{
 
-		public static int VersionMajor = 0;
-		public static int VersionMinor = 0;
-		public static int VersionRevision = 1;
-		public static string VersionName = "a";
-		public static string Version { get { return VersionMajor + "." + VersionMinor + "." + VersionRevision + VersionName; } }
-
 		[DllImport("__Internal", EntryPoint="mono_get_runtime_build_info")]
 		public extern static string GetMonoVersion();
 
+		public static string Version()
+		{
+			var asm = Assembly.GetCallingAssembly();
+			return asm.GetName().Version + " " + asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+		}
+
 		public static void Main(string[] args)
 		{
-			Terminal.Message("Nostrand", Version);
+			Terminal.Message("Nostrand", Version());
 			Terminal.Message("Mono", GetMonoVersion());
 			Console.WriteLine();
 			// Terminal.Message ("Clojure", RT.var("clojure.core", "clojure-version").invoke());
@@ -34,7 +36,7 @@ namespace Nostrand
 				RT.var(taskNS, taskVar).invoke();
 			}
 
-			LineEditor le = new LineEditor("foo");
+			LineEditor le = new LineEditor("nostrand");
 			le.AutoCompleteEvent += delegate (string a, int pos)
 			{
 				string prefix = "";
---
+++ Nostrand/Nostrand/unpatch-assembly-info.sh
@@ -0,0 +1,1 @@
+sed -i -pe "s/AssemblyInformationalVersion.*/AssemblyInformationalVersion(\"\")]/g" Properties/AssemblyInfo.cs
