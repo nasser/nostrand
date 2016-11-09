4c38468c272af682280f71c9f6c8188f2bb849e5
Ramsey Nasser <ram@nas.sr>
2016-11-09 13:31:24 -0500

    make version function take arg map

--- Functions/VersionFunction.cs
+++ Functions/VersionFunction.cs
@@ -4,13 +4,13 @@ namespace Nostrand
 	[Function("version")]
 	public class VersionFunction : AFn
 	{
-		public override object invoke()
+		public override object invoke(object arg)
 		{
 			Terminal.Message("Nostrand", Nostrand.Version());
 			Terminal.Message("Mono", Nostrand.GetMonoVersion());
 			Terminal.Message("Clojure", RT.var("clojure.core", "clojure-version").invoke());
 
-			return null;
+			return arg;
 		}
 	}
 }
