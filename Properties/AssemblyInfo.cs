33c52215c97fd17f7f1a4630f98907014486ff7d
Ramsey Nasser <ram@nas.sr>
2020-02-07 22:42:39 -0500

    Remove commit information from AssemblyInfo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("Nostrand")]
 [assembly: AssemblyDescription("Clojure on Mono")]
 [assembly: AssemblyCopyright("Ramsey Nasser")]
 [assembly: AssemblyVersion("0.2.1.0")]
-[assembly: AssemblyInformationalVersion("(master/4ff980e* Wed 05 Feb 2020 06:51:23 PM EST)")]
+[assembly: AssemblyInformationalVersion("")]
