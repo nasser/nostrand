9619224274a0bb0c70ae716a8600ef7846882b54
Ramsey Nasser <ram@nas.sr>
2020-11-29 09:56:24 -0500

    Remove old bug avoidance logic
    Does not seem necessary anymore and was causing printing issues

--- Terminal.cs
+++ Terminal.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace Nostrand
 {
 	public class Terminal
@@ -20,17 +20,7 @@ namespace Nostrand
 				Console.Write (label + " ");
 			Console.ForegroundColor = ConsoleColor.Gray;
 			Console.WriteLine (content.ToString());
-			//Console.ForegroundColor = oldColor;
 			Console.ResetColor ();
-			// due to some bug in mono we need to compute the cursor offset
-			// ourselves otherwise Mono.Terminal breaks
-			var fullMessage = label != null ? "${label} ${content}" : content.ToString();
-			int newLines = 0;
-			foreach (var line in fullMessage.Split(new char['\n']))
-			{
-				newLines += 1 + (line.Length / Console.WindowWidth);
-			}
-			Console.SetCursorPosition(0, Math.Min(Console.CursorTop+newLines, Console.WindowHeight-1));
 		}
 	}
 }
