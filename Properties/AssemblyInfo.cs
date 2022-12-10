2b88f1de06cce163555436a6feae77832c80d1bf
Ramsey Nasser <ram@nas.sr>
2022-12-10 14:38:26 -0500

    Remove assembly info

--- Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("Nostrand")]
-[assembly: AssemblyDescription("Clojure on Mono")]
-[assembly: AssemblyCopyright("Ramsey Nasser")]
-[assembly: AssemblyVersion("0.2.1.0")]
-[assembly: AssemblyInformationalVersion("")]
